04dbc732cf62828bde37ab992378741ec5116361
zeran <zeranxu@foxmail.com>
2021-03-02 10:26:40 +0800

    首次提交

--- uav_project/重构的.py
+++ uav_project/重构的.py
@@ -12,17 +12,17 @@ def read_labelme_json(path):
     shapes=np.asarray(data['shapes'][0]['points'])
     return shapes
 
-from math import *
-def calR(ω,ϕ,κ):
-    return np.asarray([cos(ϕ)*cos(κ),
-    -cos(ϕ)*sin(κ),
-    sin(ϕ),
-    cos(ω)*sin(κ)+sin(ω)*sin(ϕ)*cos(κ),
-    cos(ω)*cos(κ)-sin(ω)*sin(ϕ)*sin(κ),
-    -sin(ω)*cos(ϕ),
-    sin(ω)*sin(κ)-cos(ω)*sin(ϕ)*cos(κ),
-    sin(ω)*cos(κ)+cos(ω)*sin(ϕ)*sin(κ),
-    cos(ω)*cos(ϕ)]).reshape([3,3])
+# from math import *
+# def calR(ω,ϕ,κ):
+#     return np.asarray([cos(ϕ)*cos(κ),
+#     -cos(ϕ)*sin(κ),
+#     sin(ϕ),
+#     cos(ω)*sin(κ)+sin(ω)*sin(ϕ)*cos(κ),
+#     cos(ω)*cos(κ)-sin(ω)*sin(ϕ)*sin(κ),
+#     -sin(ω)*cos(ϕ),
+#     sin(ω)*sin(κ)-cos(ω)*sin(ϕ)*cos(κ),
+#     sin(ω)*cos(κ)+cos(ω)*sin(ϕ)*sin(κ),
+#     cos(ω)*cos(ϕ)]).reshape([3,3])
 
 
 # 用labelme标记的json文件
